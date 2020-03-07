87d610e651de0042b9529d8d67add97cf992273d
Sylvain <sylvain.rieutor@orange.fr>
2020-03-07 15:50:37 +0100

    Fix alert forms

--- users/views.py
+++ users/views.py
@@ -1,36 +1,31 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.template import loader
+from django.contrib import messages
 from .forms import UserRegisterForm
-from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
-
-from django.contrib.auth.models import User
-
 
 
 def register(request):
+    title = "Inscription"
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        
         if form.is_valid():
             form.save()
-
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Votre compte a bien été créé,<br> Vous pouvez vous connecter.')
             return redirect('login')
-    
-        else:
-            print("register problem")
-            return redirect('register')
-                  
-    return render(request, 'users/register.html')
+    else:
+        form = UserRegisterForm()
+ 
+    return render(request, 'users/register.html', {'form': form, 'title': title})
     
 def login (request):
-    return render(request,'users/login.html' )     
+    
+    return render(request,'users/login.html', {'title': "Connexion"} )     
 
 @login_required
 def logout(request):
-    return render(request,'users/logout.html' )
+    return render(request,'users/logout.html', {'title': "Déconnexion"} )
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    return render(request, 'users/profile.html', {'title': "Profil"})
